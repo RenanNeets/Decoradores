72637ef20009ddbdd5251abc2b89025a68edf1f1
Renan <130794999+RenanNeets@users.noreply.github.com>
2024-09-19 14:05:49 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+"""
+# Decoradores com parâmetros
+def fabrica_de_decoradores(a=None, b=None, c=None): #Pegua os parâmetros do decorador
+    def fabrica_de_funcoes(func): #Vai pegar a função
+        print('Decoradora 1')
+
+        def aninhada(*args, **kwargs): #Sua função que vai executar
+            print('Parâmetros do decorador, ', a, b, c)
+            print('Aninhada')
+            res = func(*args, **kwargs)
+            return res
+        return aninhada
+    return fabrica_de_funcoes
+
+
+@fabrica_de_decoradores(1, 2, 3)
+def soma(x, y):
+    return x + y
+
+
+decoradora = fabrica_de_decoradores()
+multiplica = decoradora(lambda x, y: x * y)
+
+dez_mais_cinco = soma(10, 5)
+dez_vezes_cinco = multiplica(10, 5)
+print(dez_mais_cinco, 'Dez mais cinco')
+print(dez_vezes_cinco, 'Dez vezes cinco')
+
+"""
+""""""
+
+# Ordem dos decoradores
+def parametros_decorador(nome):
+    def decorador(func):
+        print('Decorador:', nome)
+
+        def sua_nova_funcao(*args, **kwargs):
+            res = func(*args, **kwargs)
+            final = f'{res} {nome}'
+            return final
+        return sua_nova_funcao
+    return decorador
+
+
+@parametros_decorador(nome='5')
+@parametros_decorador(nome='4')
+@parametros_decorador(nome='3')
+@parametros_decorador(nome='2')
+@parametros_decorador(nome='1')
+def soma(x, y):
+    return x + y
+
+
+dez_mais_cinco = soma(10, 5)
+print(dez_mais_cinco)
---
+++ exercicio_soma_duas_listas.py
@@ -0,0 +1,12 @@
+lista_a = [10, 2, 3, 4, 5]
+lista_b = [12, 2, 3, 6, 50, 60, 70]
+lista_soma = [x + y for x, y in zip(lista_a, lista_b)]
+print(lista_soma)  # Saída: [22, 4, 6, 10, 55]
+"""
+2° Forma
+from itertools import zip_longest
+lista_a = [10, 2, 3, 4, 5]
+lista_b = [12, 2, 3, 6, 50, 60, 70]
+lista_soma = [x + y for x, y in zip_longest(lista_a, lista_b, fillvalue=0)]
+print(lista_soma)  # [22, 4, 6, 10, 55, 60, 70]
+"""
